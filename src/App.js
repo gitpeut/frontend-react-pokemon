42a9c60468192b4d28d211a75f7c524053d1672d
gitpeut <peut@peut.org>
2021-09-30 22:43:05 +0200

    met idx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,84 @@
 import React from 'react';
+import { useState, useEffect} from "react";
+import axios from 'axios';
+import Card from "./components/card/Card";
 import './App.css';
 
+
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
+const [ pokemonList, setPokemonList ] = useState({});
+const [ pokemonUrl, setPokemonUrl] = useState( 'https://pokeapi.co/api/v2/pokemon/' ) ;
+
+    useEffect (
+        ()=>{
+            async function fetchData( ){
+                let result={};
+                //console.log("fetch data ");
+                try{
+                    result = await axios.get( pokemonUrl );
+                    //console.log(result.data);
+
+                    let list = {};
+                    list.count = result.data.count;
+                    list.next  = result.data.next;
+                    list.previous = result.data.previous;
+
+                    list.idxs  = result.data.results.map( ( result )=> {
+                            const spliturl = result.url.split('/');
+                            const splitlen = spliturl.length;
+
+                            //console.log( ' spliturl ' + spliturl[ splitlen - 2 ] );
+                            return( spliturl[ splitlen -2 ] );
+                        }
+                    );
+
+                    setPokemonList( list );
+                    document.title="PokéApi"
+                }catch (e){
+                    console.log(`Fetch data from pokeapi failed ${e}`  );
+                }
+            }
+
+            if ( pokemonUrl  )fetchData();
+        },
+        [pokemonUrl]
+    );
+
+
+    return (
+    <div className="app-div">
+        <img className="logo" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="PokeApilogo"/>
+        {Object.keys(pokemonList).length <= 0 && <h3>Loading data...</h3>}
+        <div>
+            { Object.keys(pokemonList).length > 0 &&
+              pokemonList.idxs.map( ( number )=>{
+                  let key = `card${number}`;
+                  return( <Card pokemonIdx={number} key={key} /> );
+            })
+
+            }
+
+        </div>
+
+        { Object.keys(pokemonList).length > 0 &&
+            <div className="button-div">
+            <button
+                type="button"
+                disabled = { !pokemonList.previous}
+                onClick={ ()=>{
+                    setPokemonUrl( pokemonList.previous );
+                }}
+            >Previous</button>
+
+            <button
+                type="button"
+                disabled = { !pokemonList.next}
+                onClick={ ()=>{
+                    setPokemonUrl( pokemonList.next )
+                }}
+            >Next</button>
+            </div>
+        }
     </div>
   );
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,36 +1,62 @@
-import {useEffect} from 'react'
+import {useState,useEffect} from 'react'
+import axios from 'axios';
 
-
+import './Card.css';
 
 function Card( {pokemonIdx} ){
 
-const [ pokemonData, setPokemonData  ]    useState( {} );
+const [ pokemonData, setPokemonData  ]  =  useState( {} );
 
 useEffect (
     ()=>{
         async function fetchData( ){
             let result={};
-            console.log("fetch data " + pokemonIdx);
+            //console.log("fetch data " + pokemonIdx);
             try{
                 result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonIdx}/` );
-                console.log(result.data);
+                //console.log(result.data);
                 setPokemonData(
                     {
                         name: result.data.name,
                         moves: result.data.moves.length,
                         weight: result.data.weight,
-                        abilities: result.data.abilies, // array van ability objecten {
+                        abilities: result.data.abilities, // array van objecten met een ability object {name:,url:} erin
+                        imageUrl: result.data.sprites.front_shiny,
                     }
                 );
+                //console.log( pokemonData );
 
             }catch (e){
-                console.log(`Fetch data from openweather.org failed ${e}`  );
+                console.log(`Fetch data from pokeapi failed ${e}`  );
             }
         }
 
-        if ( location !== '' )fetchData();
+        if ( pokemonIdx >= 0  )fetchData();
     },
-        [PokemonIdx]
+        [pokemonIdx]
+);
+
+return(
+    <div className="card" key={pokemonData.name}>
+        {Object.keys(pokemonData).length > 0 &&
+        <>
+            <h2>{pokemonIdx} {pokemonData.name}</h2>
+            <img src={pokemonData.imageUrl} alt={pokemonData.name}/>
+            <h3>Moves: {pokemonData.moves}</h3>
+            <h3>Weight: {pokemonData.weight}</h3>
+            <h4>Abilities</h4>
+        </>
+        }
+        { Object.keys(pokemonData).length > 0 &&
+        pokemonData.abilities.map((able) => {
+                    //console.log(`ability ${able.ability.name}`);
+                    return(
+                    <p key={able.ability.name}>{able.ability.name}</p>
+                    );
+                })
+
+        }
+    </div>
 );
 
 }
